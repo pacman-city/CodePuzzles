2ef4df3d758e1282e8b3073124e664aff46b7f14
Евгений Клюев <craftycoderpro@gmail.com>
2022-09-08 15:33:05 +0300

    add comments in parentheses

--- other/true_parentheses.py
+++ other/true_parentheses.py
@@ -11,17 +11,17 @@ for s in str:
     if len(current) == 0 and s in ')}]':
         print(False)
         break
-    elif s in '({[':
+    elif s in '({[':  # добавляем все открывающие скобки в current
         current.append(s)
-    elif (s == ')' and current[-1] != '(' or
+    elif (s == ')' and current[-1] != '(' or  # проверка - если закрывающая скобка - неправильного типа
           s == '}' and current[-1] != '{' or
           s == ']' and current[-1] != '['):
         print(False)
         break
     else:
-        current.pop(-1)
+        current.pop(-1)  # если скобка правильно закрыта - убираем из current
 else:
-    if len(current):
+    if len(current):  # если остались незакрытые скобки
         print(False)
     else:
-        print(True)
+        print(True)  # если пустая строка
